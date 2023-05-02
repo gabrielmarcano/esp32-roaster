808da208f1ca4498286281c4efc426e227cf41d3
Gabriel Marcano <gabrielmarcano120@gmail.com>
2023-05-01 20:53:48 -0400

    timer & switch logic working

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,13 @@
 #include "max6675.h"
 
 #define DHT_PIN 18
-#define MOTOR_PIN 2 // por prueba colocamos el led interno
+#define MOTOR1_PIN 2 // 25, but internal LED (2) for debugging
+#define MOTOR2_PIN 26
+#define MOTOR3_PIN 27
+#define BUZZER_PIN 14
+#define MINUTES_12 36
+#define MINUTES_15 34
+#define MINUTES_18 35
 
 AsyncWebServer server(80);          // Create AsyncWebServer object on port 80
 AsyncEventSource events("/events"); // Create an Event Source on /events
@@ -25,22 +31,31 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // addr, width (16), height(2) -> 16x2 LCD
 dht11 DHT11;
 MAX6675 thermocouple(5, 23, 19); // SCK, CS, SO
 
-bool motorStatus = LOW;
+int temperature, humidity;
+
+const char *mainTitle = "Tostador";
+
+int timerCount, counter;
+bool timerIsOn = false;
+bool timerResponseIsActive = false;
+int min12, min15, min18;
 
 // Get Sensor Readings and return JSON object
 String getSensorReadings()
 {
   // Update readings
   DHT11.read(DHT_PIN);
-  int temperature = (int)thermocouple.readCelsius();
+
+  humidity = DHT11.humidity;
+  temperature = (int)thermocouple.readCelsius();
 
   // LCD
-  lcd.setCursor(0, 1); // Set the cursor to column 0, line 1
-  lcd.printf("T: %dC H: %d%%   ", temperature, DHT11.humidity);
+  lcd.setCursor(0, 1); // Set the cursor to column 0, row 1
+  lcd.printf("T: %dC H: %d%%   ", temperature, humidity);
 
   // JSON
   readings["temperature"] = String(temperature);
-  readings["humidity"] = String(DHT11.humidity);
+  readings["humidity"] = String(humidity);
   String jsonString = JSON.stringify(readings);
   return jsonString;
 }
@@ -70,6 +85,9 @@ void initWifi(const char *ssid, const char *password)
 
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
 }
 
 // Initialize LCD
@@ -81,14 +99,9 @@ void initLCD(const char *title)
   lcd.print(title);
 }
 
-void setup()
+// Initialize Server
+void initServer()
 {
-  Serial.begin(115200);
-  pinMode(MOTOR_PIN, OUTPUT);
-  initLCD("Tostador d cacao");
-  initWifi("GUEST", "88426259");
-  initSPIFFS();
-
   // Web Server Root URL
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/index.html", "text/html"); });
@@ -116,34 +129,177 @@ void setup()
   server.begin();
 }
 
+// Format seconds into mm:ss
+String formatTime(int minutes, int seconds)
+{
+  String time;
+  time = (minutes < 10) ? time + String("0") + String(minutes) : time + String(minutes);
+  time = time + String(":");
+  time = (seconds < 10) ? time + String("0") + String(seconds) : time + String(seconds);
+  if (min12)
+  {
+    time = time + String(" Maní");
+  }
+  else if (min15)
+  {
+    time = time + String(" Café");
+  }
+  else if (min18)
+  {
+    time = time + String(" Cacao");
+  }
+  time = time + String("           ");
+  return time;
+}
+
+// Make noise with the buzzer
+void handleBuzzer()
+{
+  tone(BUZZER_PIN, 440);
+  // tone(BUZZER_PIN, 494, 1000);
+  // tone(BUZZER_PIN, 523, 1000);
+}
+
+// Handle all timer logic with the 3-state switch
+void handleTimer()
+{
+  if (timerCount != 0 && timerIsOn)
+  {
+    if (counter >= 1)
+    {
+      int minutes = floor(counter / 60);
+      int remainingSeconds = counter - minutes * 60;
+
+      lcd.setCursor(0, 0);
+      lcd.print(formatTime(minutes, remainingSeconds));
+
+      counter--;
+    }
+
+    if (counter < 1)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("00:00");
+      timerIsOn = false;
+      timerResponseIsActive = true;
+      lcd.setCursor(0, 0);
+      lcd.print(mainTitle);
+    }
+  }
+
+  // When switch is moved to OFF, then turn off the response
+  if (!min12 && !min15 && !min18)
+  {
+    timerResponseIsActive = false;
+    noTone(BUZZER_PIN);
+  }
+
+  // If there is a switch after the counter finished, handle the response
+  if (timerResponseIsActive)
+  {
+    handleBuzzer();
+    digitalWrite(MOTOR2_PIN, HIGH);
+    digitalWrite(MOTOR3_PIN, HIGH);
+    Serial.println("motor 2 y 3 are ON");
+  }
+}
+
+// Handle motor Logic
+void handleTemperature()
+{
+  // 100-150-190 but 30-40-50 for debugging
+  int tempLimit = min12 ? 30 : min15 ? 40
+                           : min18   ? 50
+                                     : INFINITY;
+
+  // TODO: Refactor to detect change from <tempLimit to >=tempLimit
+  if (temperature >= tempLimit)
+  {
+    digitalWrite(MOTOR1_PIN, HIGH);
+  }
+
+  // -- No need to detect change since a minute switch can be set after the motor started
+  // -- If a timer is already running, don't set it again
+  if (temperature >= tempLimit && !timerIsOn)
+  {
+    // Only set a timer if there is a switch selected
+    bool thereIsAMinuteSwitch = min12 || min15 || min18;
+    if (thereIsAMinuteSwitch)
+    {
+      // 12-15-18 but 0.3 for debugging
+      counter = min12 ? 0.3 * 60 : min15 ? 0.3 * 60
+                               : min18   ? 0.3 * 60
+                                         : 0;
+      timerCount++;
+      timerIsOn = true;
+    }
+
+    // TODO: add +1 minute or -1 minute with 2 extra inputs
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(MOTOR1_PIN, OUTPUT);
+  pinMode(MOTOR2_PIN, OUTPUT);
+  pinMode(MOTOR3_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(MINUTES_12, INPUT);
+  pinMode(MINUTES_15, INPUT);
+  pinMode(MINUTES_18, INPUT);
+
+  initLCD(mainTitle);
+  initWifi("GUEST", "88426259");
+  initSPIFFS();
+  initServer();
+
+  delay(2000);
+}
+
 void loop()
 {
+  // Get switch readings
+  min12 = digitalRead(MINUTES_12);
+  min15 = digitalRead(MINUTES_15);
+  min18 = digitalRead(MINUTES_18);
+
+  // Send Events to the client with the Sensor Readings Every 4 seconds
   events.send("ping", NULL, millis());
   events.send(getSensorReadings().c_str(), "new_readings", millis());
 
-  delay(4000);
+  // DEBUG TIME
+  Serial.println("Timer data:");
+  Serial.println(counter);
+  Serial.println(timerIsOn);
+  Serial.println(timerCount);
+
+  handleTemperature();
+  handleTimer();
+
+  // DEBUG SWITCH
+  Serial.println("Switch 1 2 3:");
+  Serial.println(min12);
+  Serial.println(min15);
+  Serial.println(min18);
+  Serial.println();
+
+  delay(1000);
 }
 
 /*
-TIMER
 
-T1 = 12m
-T2 = 15m
-T3 = 18m
+MANUAL WAIT
 
-else = APAGAR EL BUZZER
+unsigned long readingsLastTime = 0;
+unsigned long readingsDelay = 1000;
 
-HACER LOGICA DE DECONTEO
+if ((millis() - readingsLastTime) > readingsDelay)
+{
+  // Things here will repeat every "readingsDelay" seconds
 
-0 O MENOR SE APAGA - ENCIENDE MOTOR 2
-*/
-
-/*
-
-LIMITE TEMPERATURA 240
-
-motor enciente en 190
-
-MOTOR NO APAGA
+  readingsLastTime = millis();
+}
 
 */
