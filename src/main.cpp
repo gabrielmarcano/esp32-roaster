0a2cb8e4175919d011747754ac785eca336b7673
Gabriel Marcano <gabrielmarcano120@gmail.com>
2023-08-07 17:38:17 -0400

    add OTA service

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
+#include <AsyncElegantOTA.h>
 
 #include "SPIFFS.h"
 
@@ -218,6 +219,9 @@ void initServer()
     // and set reconnect delay to 1 second
     client->send("hello!", NULL, millis(), 10000); });
   server.addHandler(&events);
+
+  // Start OTA service
+  AsyncElegantOTA.begin(&server);
 
   // Start server
   server.begin();
